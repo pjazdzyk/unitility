ecc837ff94e4d36d2b2f00cebce30a6b9fdff2c8
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2023-01-24 21:12:41 +0100

    ad fail tests

--- src/test/java/com/synerset/unitsystem/temperature/TemperatureTest.java
+++ src/test/java/com/synerset/unitsystem/temperature/TemperatureTest.java
@@ -5,47 +5,53 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.withPrecision;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TemperatureTest {
 
     // Given
-    private static final double TEST_TEMP_VALUE = 293.15;
-    private static final Temperature TEST_KELVIN_TEMP = Temperature.kelvin(TEST_TEMP_VALUE).getOrElseThrow(
+    private static final Temperature TEST_KELVIN_TEMP = Temperature.kelvin(293.15).getOrElseThrow(
             () -> new IllegalStateException("Unit tests: Invalid temperature"));
-
     private static final double MATH_ACCURACY = 1E-13;
 
     @Test
     @DisplayName("should convert Kelvin to Celsius and vice versa")
     void shouldConvertKelvinToCelsiusAndViceVersa() {
         // When
-        Temperature actualUnitCelsius = TEST_KELVIN_TEMP.toCelsius();
-        double actualValueInCelsius = actualUnitCelsius.getValue();
-
-        Temperature actualUnitKelvin = actualUnitCelsius.toKelvin();
-        double actualValueInKelvin = actualUnitKelvin.getValue();
+        Celsius actualUnitCelsius = TEST_KELVIN_TEMP.toCelsius();
+        Kelvin actualUnitKelvin = actualUnitCelsius.toKelvin();
 
         // Then
-        assertThat(actualValueInKelvin).isEqualTo(TEST_TEMP_VALUE);
-        double expectedValueInCelsius = 20.0;
-        assertThat(actualValueInCelsius).isEqualTo(expectedValueInCelsius);
+        assertThat(actualUnitKelvin).isEqualTo(TEST_KELVIN_TEMP);
+        Celsius expectedValueInCelsius = Temperature.celsius(20.0);
+        assertThat(actualUnitCelsius).isEqualTo(expectedValueInCelsius);
     }
 
     @Test
     @DisplayName("should convert Kelvin to Fahrenheit and vice versa")
     void shouldConvertKelvinsToFahrenheitAndViceVersa() {
         // When
-        Temperature actualUnitFahrenheit = TEST_KELVIN_TEMP.toFahrenheit();
+        Fahrenheit actualUnitFahrenheit = TEST_KELVIN_TEMP.toFahrenheit();
         double actualValueInFahrenheit = actualUnitFahrenheit.getValue();
-
-        Temperature actualUnitKelvin = actualUnitFahrenheit.toKelvin();
-        double actualValueInKelvin = actualUnitKelvin.getValue();
+        Kelvin actualUnitKelvin = actualUnitFahrenheit.toKelvin();
 
         // Then
-        assertThat(actualValueInKelvin).isEqualTo(TEST_TEMP_VALUE);
+        assertThat(actualUnitKelvin).isEqualTo(TEST_KELVIN_TEMP);
         double expectedValueInFahrenheit = 68.0;
         assertThat(actualValueInFahrenheit).isEqualTo(expectedValueInFahrenheit, withPrecision(MATH_ACCURACY));
     }
 
+    @Test
+    @DisplayName("should return InvalidTemperature when negative temperature in Kelvin is given")
+    void shouldReturnInvalidTemperatureWhenNegativeTempInKelvinIsGiven(){
+        // Then
+        assertThat(Temperature.kelvin(-1).getLeft()).isInstanceOf(InvalidTemperature.class);
+    }
 
+    @Test
+    @DisplayName("should return InvalidTemperature when unphysical temperature in Celsius is given")
+    void shouldReturnInvalidTemperatureWhenUnphysicalTempInCelsiusIsGiven(){
+        // Then
+        assertThatThrownBy(()->Temperature.celsius(-1000)).isInstanceOf(IllegalStateException.class);
+    }
 }
