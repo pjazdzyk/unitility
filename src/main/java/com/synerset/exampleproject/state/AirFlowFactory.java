3aaa5d860923a5fc8bf10ee6edf2f480b14a6f09
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2023-02-15 21:42:01 +0100

    changed Either ifs to mapping in flowFactory (example project)

--- src/main/java/com/synerset/exampleproject/state/AirFlowFactory.java
+++ src/main/java/com/synerset/exampleproject/state/AirFlowFactory.java
@@ -2,16 +2,16 @@ package com.synerset.exampleproject.state;
 
 import com.synerset.exampleproject.common.Defaults;
 import com.synerset.exampleproject.properties.DryAirProperties;
-import com.synerset.unitsystem.density.InvalidDensity;
-import com.synerset.unitsystem.density.KiloGramPerCubicMeter;
 import com.synerset.unitsystem.massflow.MassFlow;
 import com.synerset.unitsystem.pressure.Pressure;
-import com.synerset.unitsystem.specificheat.InvalidSpecificHeat;
-import com.synerset.unitsystem.specificheat.KiloJoulePerKilogramKelvin;
 import com.synerset.unitsystem.temperature.Temperature;
 import io.vavr.control.Either;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AirFlowFactory {
+
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
 
     private final DryAirProperties dryAirProperties;
 
@@ -20,22 +20,25 @@ public class AirFlowFactory {
     }
 
     public Either<InvalidFlow, FlowOfDryAir> createFlowOfDryAir(Pressure pressure, Temperature temperature, MassFlow massFlow) {
-        Either<InvalidDensity, KiloGramPerCubicMeter> density = dryAirProperties.density(temperature, pressure);
-        if (density.isLeft()) {
-            return Either.left(new InvalidFlow());
-        }
-        Either<InvalidSpecificHeat, KiloJoulePerKilogramKelvin> specHeat = dryAirProperties.specificHeat(temperature);
-        if(specHeat.isLeft()){
-            return Either.left(new InvalidFlow());
-        }
-        DryAirState dryAirState = new DryAirState(
-                pressure.toPascal(),
-                temperature.toCelsius(),
-                density.get(),
-                specHeat.get()
-        );
-        return Either.right(new FlowOfDryAir(dryAirState, massFlow.toKiloGramPerSecond()));
+        LOGGER.info("Trying to create instance of dry air");
+        return dryAirProperties.density(temperature, pressure)
+                .mapLeft(l -> {
+                    LOGGER.error("Flow cannot be created due to invalid density.");
+                    return new InvalidFlow();
+                })
+                .flatMap(density -> dryAirProperties.specificHeat(temperature)
+                        .mapLeft(l -> {
+                            LOGGER.error("Flow cannot be created due to invalid specific heat.");
+                            return new InvalidFlow();
+                        })
+                        .map(specHeat -> new DryAirState(pressure.toPascal(),
+                                temperature.toCelsius(),
+                                density,
+                                specHeat))
+                )
+                .map(dryAir -> new FlowOfDryAir(dryAir, massFlow.toKiloGramPerSecond()));
     }
+
 
     public Either<InvalidFlow, FlowOfDryAir> createFlowOfDryAir(Temperature temperature, MassFlow massFlow) {
         return createFlowOfDryAir(Defaults.DEF_ATM_PRESSURE, temperature, massFlow);
