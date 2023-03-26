9a9f17ffbf7d80ac35e39fef3e271223486969d2
Piotr Jazdzyk <piotr.jazdzyk@o2.pl>
2023-03-26 14:41:42 +0200

    move InvalidProperty merge to FluidValidators

--- singleunittype/src/main/java/com/synerset/exampleproject/fluidproperties/InvalidProperty.java
+++ singleunittype/src/main/java/com/synerset/exampleproject/fluidproperties/InvalidProperty.java
@@ -9,10 +9,4 @@ public record InvalidProperty(PhysicalQuantity<?> physicalQuantity, String msg) 
         this(null, msg);
     }
 
-    public static InvalidProperty merge(Seq<InvalidProperty> invalidProperties) {
-        StringBuilder stringBuilder = new StringBuilder();
-        invalidProperties.forEach(invalidProp -> stringBuilder.append("\n").append(invalidProp.msg));
-        return new InvalidProperty(invalidProperties.last().physicalQuantity, stringBuilder.toString());
-    }
-
 }
