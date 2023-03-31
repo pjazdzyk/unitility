01beb0e0084686c83a6dc993b1929603ac482752
Piotr Jazdzyk <piotr.jazdzyk@o2.pl>
2023-03-31 20:33:13 +0200

    add HumidityRatio and associated unit tests

---
+++ second-concept/src/test/java/com/synerset/unitsystem/humidityratio/HumidityRatioTest.java
@@ -0,0 +1,24 @@
+package com.synerset.unitsystem.humidityratio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+public class HumidityRatioTest {
+
+    @Test
+    @DisplayName("should properly convert from kg/kg to lb/lb")
+    public void shouldProperlyConvertFromKgKgToLbLb() {
+        // Given
+        double initialValue = 0.015;
+        HumidityRatio initialHumidityRatio = HumidityRatio.of(initialValue, HumidityRatioUnits.KILOGRAM_PER_KILOGRAM);
+
+        // When
+        HumidityRatio actualInLbPerLb = initialHumidityRatio.toUnit(HumidityRatioUnits.POUND_PER_POUND);
+        HumidityRatio actualInKgPerKg = actualInLbPerLb.toUnit(HumidityRatioUnits.KILOGRAM_PER_KILOGRAM);
+
+        // Then
+        HumidityRatio expectedInLbPerLb = HumidityRatio.of(0.033069339826536, HumidityRatioUnits.POUND_PER_POUND);
+        assertThat(actualInKgPerKg.getValue()).isEqualTo(initialValue);
+        assertThat(actualInKgPerKg.getUnit()).isEqualTo(HumidityRatioUnits.KILOGRAM_PER_KILOGRAM);
+    }
+}
