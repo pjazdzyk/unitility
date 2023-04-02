0e76b663561f8763b4db8a032fb16843d9f2c17c
Piotr Jazdzyk <piotr.jazdzyk@o2.pl>
2023-04-02 17:29:27 +0200

    add Force quantity and associated unit tests

---
+++ src/main/java/com/synerset/unitility/unitsystem/force/ForceUnits.java
@@ -0,0 +1,46 @@
+package com.synerset.unitility.unitsystem.force;
+
+import com.synerset.unitility.unitsystem.Unit;
+
+import java.util.function.DoubleUnaryOperator;
+
+public enum ForceUnits implements Unit<Force> {
+
+    NEWTON("N", val -> val, val -> val),
+    KILO_NEWTON("kN", val -> val * 1000.0, val -> val / 1000.0),
+    KILOPOND("kp", val -> val * 9.80665, val -> val / 9.80665),
+    DYNE("dyn", val -> val * 0.00001, val -> val / 0.00001),
+    POUND_FORCE("lbf", val -> val * 4.4482216152605, val -> val / 4.4482216152605),
+    POUNDAL("pdl", val -> val * 0.138254954376, val -> val / 0.138254954376);
+
+    private final String symbol;
+    private final DoubleUnaryOperator toBaseConverter;
+    private final DoubleUnaryOperator fromBaseToUnitConverter;
+
+    ForceUnits(String symbol, DoubleUnaryOperator toBaseConverter, DoubleUnaryOperator fromBaseToUnitConverter) {
+        this.symbol = symbol;
+        this.toBaseConverter = toBaseConverter;
+        this.fromBaseToUnitConverter = fromBaseToUnitConverter;
+    }
+
+    @Override
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public Unit<Force> getBaseUnit() {
+        return NEWTON;
+    }
+
+    @Override
+    public double toBaseUnit(double valueInThisUnit) {
+        return toBaseConverter.applyAsDouble(valueInThisUnit);
+    }
+
+    @Override
+    public double fromBaseToThisUnit(double valueInBaseUnit) {
+        return fromBaseToUnitConverter.applyAsDouble(valueInBaseUnit);
+    }
+
+}
---
+++ src/test/java/com/synerset/unitility/unitsystem/force/ForceTest.java
@@ -0,0 +1,89 @@
+package com.synerset.unitility.unitsystem.force;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+class ForceTest {
+
+    @Test
+    @DisplayName("should convert N to kN and vice versa")
+    public void shouldProperlyConvertNewtonsToKiloNewtons() {
+        // Given
+        Force initialForce = Force.ofNewtons(1000);
+        // When
+        Force actualInKiloNewtons = initialForce.toKiloNewtons();
+        Force actualInNewtons = actualInKiloNewtons.toBaseUnit();
+
+        // Then
+        Force expectedInKiloNewtons = Force.ofKiloNewtons(1);
+        assertThat(actualInKiloNewtons).isEqualTo(expectedInKiloNewtons);
+        assertThat(actualInNewtons).isEqualTo(initialForce);
+    }
+
+    @Test
+    @DisplayName("should convert N to kp and vice versa")
+    public void shouldProperlyConvertNewtonsToKiloPonds() {
+        // Given
+        Force initialForce = Force.ofNewtons(1000);
+
+        // When
+        Force actualInKiloPonds = initialForce.toKiloponds();
+        Force actualInNewtons = actualInKiloPonds.toNewtons();
+
+        // Then
+        Force expectedInKiloPonds = Force.ofKiloponds(101.97162129779283);
+        assertThat(actualInKiloPonds.getValue()).isEqualTo(expectedInKiloPonds.getValue(), withPrecision(1E-14));
+        assertThat(actualInNewtons.getValue()).isEqualTo(initialForce.getValue(), withPrecision(1E-13));
+    }
+
+    @Test
+    @DisplayName("should convert N to dyn and vice versa")
+    public void shouldProperlyConvertNewtonsToDynes() {
+        // Given
+        Force initialForce = Force.ofNewtons(1.0);
+
+        // When
+        Force actualInDyn = initialForce.toDynes();
+        Force actualInNewtons = actualInDyn.toNewtons();
+
+        // Then
+        Force expectedInDyn = Force.ofDynes(100_000.0);
+        assertThat(actualInDyn.getValue()).isEqualTo(expectedInDyn.getValue(), withPrecision(1E-11));
+        assertThat(actualInNewtons.getValue()).isEqualTo(initialForce.getValue(), withPrecision(1E-15));
+    }
+
+    @Test
+    @DisplayName("should convert N to lbf and vice versa")
+    public void shouldProperlyConvertNewtonsToPoundForce() {
+        // Given
+        Force initialForce = Force.ofNewtons(1000);
+
+        // When
+        Force actualInPoundForce = initialForce.toPoundForce();
+        Force actualInNewtons = actualInPoundForce.toNewtons();
+
+        // Then
+        Force expectedInPoundForce = Force.ofPoundForce(224.8089430997105);
+        assertThat(actualInPoundForce.getValue()).isEqualTo(expectedInPoundForce.getValue(), withPrecision(1E-15));
+        assertThat(actualInNewtons).isEqualTo(initialForce);
+    }
+
+    @Test
+    @DisplayName("should convert N to pdl and vice versa")
+    public void shouldProperlyConvertNewtonsToPoundal() {
+        // Given
+        Force initialForce = Force.ofNewtons(1000);
+        // When
+        Force actualInPoundal = initialForce.toPoundal();
+        Force actualInNewtons = actualInPoundal.toBaseUnit();
+
+        // Then
+        Force expectedInPoundal = Force.ofPoundal(7233.013851209894);
+        assertThat(actualInPoundal.getValue()).isEqualTo(expectedInPoundal.getValue(), withPrecision(1E-12));
+        assertThat(actualInNewtons).isEqualTo(initialForce);
+    }
+
+}
