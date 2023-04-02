30ee633944c5e2a59a1c1c884859ffc098687838
Piotr Jazdzyk <piotr.jazdzyk@o2.pl>
2023-04-02 18:29:45 +0200

    add Area quantity and associated unit tests

---
+++ src/main/java/com/synerset/unitility/unitsystem/area/Area.java
@@ -0,0 +1,157 @@
+package com.synerset.unitility.unitsystem.area;
+
+import com.synerset.unitility.unitsystem.BareQuantity;
+import com.synerset.unitility.unitsystem.PhysicalQuantity;
+import com.synerset.unitility.unitsystem.Unit;
+import com.synerset.unitility.unitsystem.utils.ValueFormatter;
+
+import java.util.Objects;
+
+public final class Area implements PhysicalQuantity<Area> {
+
+    public static final Area PHYSICAL_MIN_LIMIT = Area.ofSquareMeters(0);
+
+    public static final byte TO_STRING_PRECISION = 3;
+
+    private final double value;
+    private final Unit<Area> unit;
+
+    private Area(double value, Unit<Area> unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    @Override
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public Unit<Area> getUnit() {
+        return unit;
+    }
+
+    @Override
+    public Area toBaseUnit() {
+        double valueInSquareMeters = unit.toBaseUnit(value);
+        return Area.of(valueInSquareMeters, AreaUnits.SQUARE_METER);
+    }
+
+    @Override
+    public Area toUnit(Unit<Area> targetUnit) {
+        double valueInSquareMeters = unit.toBaseUnit(value);
+        double valueInTargetUnit = targetUnit.fromBaseToThisUnit(valueInSquareMeters);
+        return Area.of(valueInTargetUnit, targetUnit);
+    }
+
+    // Custom converter methods, for most popular units
+    public Area toSquareMeters() {
+        return toUnit(AreaUnits.SQUARE_METER);
+    }
+
+    public Area toSquareKilometers() {
+        return toUnit(AreaUnits.SQUARE_KILOMETER);
+    }
+
+    public Area toSquareCentimeters() {
+        return toUnit(AreaUnits.SQUARE_CENTIMETER);
+    }
+
+    public Area toSquareMillimeters() {
+        return toUnit(AreaUnits.SQUARE_MILLIMETER);
+    }
+
+    public Area toAres() {
+        return toUnit(AreaUnits.ARE);
+    }
+
+    public Area toHectares() {
+        return toUnit(AreaUnits.HECTARE);
+    }
+
+    public Area toSquareInches() {
+        return toUnit(AreaUnits.SQUARE_INCH);
+    }
+
+    public Area toSquareFeet() {
+        return toUnit(AreaUnits.SQUARE_FOOT);
+    }
+
+    public Area toSquareYards() {
+        return toUnit(AreaUnits.SQUARE_YARD);
+    }
+
+    public Area toAcres() {
+        return toUnit(AreaUnits.ACRE);
+    }
+
+    public Area toSquareMiles() {
+        return toUnit(AreaUnits.SQUARE_MILE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Area area = (Area) o;
+        return Double.compare(area.value, value) == 0 && unit.equals(area.unit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, unit);
+    }
+
+    @Override
+    public String toString() {
+        return ValueFormatter.formatDoubleToRelevantPrecision(value, TO_STRING_PRECISION) + " " + unit.getSymbol();
+    }
+
+    public static Area of(double value, Unit<Area> unit) {
+        return new Area(value, unit);
+    }
+
+    public static Area ofSquareMeters(double value) {
+        return new Area(value, AreaUnits.SQUARE_METER);
+    }
+
+    public static Area ofSquareKilometers(double value) {
+        return new Area(value, AreaUnits.SQUARE_KILOMETER);
+    }
+
+    public static Area ofSquareCentimeters(double value) {
+        return new Area(value, AreaUnits.SQUARE_CENTIMETER);
+    }
+
+    public static Area ofSquareMillimeters(double value) {
+        return new Area(value, AreaUnits.SQUARE_MILLIMETER);
+    }
+
+    public static Area ofAres(double value) {
+        return new Area(value, AreaUnits.ARE);
+    }
+
+    public static Area ofHectares(double value) {
+        return new Area(value, AreaUnits.HECTARE);
+    }
+
+    public static Area ofSquareInches(double value) {
+        return new Area(value, AreaUnits.SQUARE_INCH);
+    }
+
+    public static Area ofSquareFeet(double value) {
+        return new Area(value, AreaUnits.SQUARE_FOOT);
+    }
+
+    public static Area ofSquareYards(double value) {
+        return new Area(value, AreaUnits.SQUARE_YARD);
+    }
+
+    public static Area ofAcres(double value) {
+        return new Area(value, AreaUnits.ACRE);
+    }
+
+    public static Area ofSquareMiles(double value) {
+        return new Area(value, AreaUnits.SQUARE_MILE);
+    }
+}
---
+++ src/test/java/com/synerset/unitility/unitsystem/area/AreaTest.java
@@ -0,0 +1,171 @@
+package com.synerset.unitility.unitsystem.area;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+class AreaTest {
+
+    @Test
+    @DisplayName("should convert m² to km² and vice versa")
+    public void shouldProperlyConvertSquareMetersToSquareKilometers() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(1_000_000);
+
+        // When
+        Area actualInSquareKilometers = initialArea.toSquareKilometers();
+        Area actualInSquareMeters = actualInSquareKilometers.toBaseUnit();
+
+        // Then
+        Area expectedInSquareKilometers = Area.ofSquareKilometers(1);
+        assertThat(actualInSquareKilometers).isEqualTo(expectedInSquareKilometers);
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+    @Test
+    @DisplayName("should convert m² to cm² and vice versa")
+    public void shouldProperlyConvertSquareMetersToSquareCentimeters() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(1);
+
+        // When
+        Area actualInSquareCentimeters = initialArea.toSquareCentimeters();
+        Area actualInSquareMeters = actualInSquareCentimeters.toBaseUnit();
+
+        // Then
+        Area expectedInSquareCentimeters = Area.ofSquareCentimeters(10_000);
+        assertThat(actualInSquareCentimeters).isEqualTo(expectedInSquareCentimeters);
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+    @Test
+    @DisplayName("should convert m² to mm² and vice versa")
+    public void shouldProperlyConvertSquareMetersToSquareMillimeters() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(1);
+
+        // When
+        Area actualInSquareMillimeters = initialArea.toSquareMillimeters();
+        Area actualInSquareMeters = actualInSquareMillimeters.toBaseUnit();
+
+        // Then
+        Area expectedInSquareMillimeters = Area.ofSquareMillimeters(1_000_000);
+        assertThat(actualInSquareMillimeters).isEqualTo(expectedInSquareMillimeters);
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+    @Test
+    @DisplayName("should convert m² to ar and vice versa")
+    public void shouldProperlyConvertSquareMetersToAres() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(1);
+
+        // When
+        Area actualInAres = initialArea.toAres();
+        Area actualInSquareMeters = actualInAres.toBaseUnit();
+
+        // Then
+        Area expectedInAres = Area.ofAres(0.01);
+        assertThat(actualInAres).isEqualTo(expectedInAres);
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+    @Test
+    @DisplayName("should convert m² to ha and vice versa")
+    public void shouldProperlyConvertSquareMetersToHectares() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(10_000);
+
+        // When
+        Area actualInHectares = initialArea.toHectares();
+        Area actualInSquareMeters = actualInHectares.toSquareMeters();
+
+        // Then
+        Area expectedInHectares = Area.ofHectares(1);
+        assertThat(actualInHectares).isEqualTo(expectedInHectares);
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+    @Test
+    @DisplayName("should convert m² to in² and vice versa")
+    public void shouldProperlyConvertSquareMetersToSquareInches() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(1);
+
+        // When
+        Area actualInSquareInches = initialArea.toSquareInches();
+        Area actualInSquareMeters = actualInSquareInches.toBaseUnit();
+
+        // Then
+        Area expectedInSquareInches = Area.ofSquareInches(1550.0031000062);
+        assertThat(actualInSquareInches.getValue()).isEqualTo(expectedInSquareInches.getValue(), withPrecision(2E-13));
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+    @Test
+    @DisplayName("should convert m² to ft² and vice versa")
+    public void shouldProperlyConvertSquareMetersToSquareFeet() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(100);
+
+        // When
+        Area actualInSquareFeet = initialArea.toSquareFeet();
+        Area actualInSquareMeters = actualInSquareFeet.toBaseUnit();
+
+        // Then
+        Area expectedInSquareFeet = Area.ofSquareFeet(1076.391041670972);
+        assertThat(actualInSquareFeet.getValue()).isEqualTo(expectedInSquareFeet.getValue(), withPrecision(1E-15));
+        assertThat(actualInSquareMeters.getValue()).isEqualTo(initialArea.getValue(), withPrecision(1E-14));
+    }
+
+    @Test
+    @DisplayName("should convert m² to yd² and vice versa")
+    public void shouldProperlyConvertSquareMetersToSquareYards() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(100);
+
+        // When
+        Area actualInSquareYards = initialArea.toSquareYards();
+        Area actualInSquareMeters = actualInSquareYards.toBaseUnit();
+
+        // Then
+        Area expectedInSquareYards = Area.ofSquareYards(119.599004630108);
+        assertThat(actualInSquareYards.getValue()).isEqualTo(expectedInSquareYards.getValue(), withPrecision(1E-13));
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+    @Test
+    @DisplayName("should convert m² to ac and vice versa")
+    public void shouldProperlyConvertSquareMetersToAcres() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(100);
+
+        // When
+        Area actualInAcres = initialArea.toAcres();
+        Area actualInSquareMeters = actualInAcres.toBaseUnit();
+
+        // Then
+        Area expectedInAcres = Area.ofSquareYards(0.0247105381467165);
+        assertThat(actualInAcres.getValue()).isEqualTo(expectedInAcres.getValue(), withPrecision(1E-15));
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+    @Test
+    @DisplayName("should convert m² to mi² and vice versa")
+    public void shouldProperlyConvertSquareMetersToSquareMiles() {
+        // Given
+        Area initialArea = Area.ofSquareMeters(100_000);
+
+        // When
+        Area actualInSquareMiles = initialArea.toSquareMiles();
+        Area actualInSquareMeters = actualInSquareMiles.toBaseUnit();
+
+        // Then
+        Area expectedInSquareMiles = Area.ofSquareMiles(0.0386102158542446);
+        assertThat(actualInSquareMiles.getValue()).isEqualTo(expectedInSquareMiles.getValue(), withPrecision(1E-15));
+        assertThat(actualInSquareMeters).isEqualTo(initialArea);
+    }
+
+}
