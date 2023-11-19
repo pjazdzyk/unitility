3090c7ac1661bc28bb4139e85098f626086b94f0
Piotr Jazdzyk <piotr.jazdzyk@o2.pl>
2023-11-19 11:15:54 +0100

    feat(SNSUNI-52): remove unused deserialization exception class

--- unitility-jackson/src/main/java/com/synerset/jackson/exceptions/UnitSystemParamDeserializationException.java
+++
@@ -1,7 +0,0 @@
-package com.synerset.jackson.exceptions;
-
-public class UnitSystemParamDeserializationException extends RuntimeException {
-    public UnitSystemParamDeserializationException(String message) {
-        super(message);
-    }
-}
