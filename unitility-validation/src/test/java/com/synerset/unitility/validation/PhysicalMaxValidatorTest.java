317bf6ce6c0a9340333bfa3fbfcce26f25d9ecff
Piotr Jazdzyk <piotr.jazdzyk@o2.pl>
2024-05-22 21:38:28 +0200

    SNSUNI-98: Update dependencies version (eg. SpringBoot 3.2.5, Quarkus 3.10.1), removed glassfish jakarta.el dependency, fixed validation tests

--- unitility-validation/src/test/java/com/synerset/unitility/validation/PhysicalMaxValidatorTest.java
+++ unitility-validation/src/test/java/com/synerset/unitility/validation/PhysicalMaxValidatorTest.java
@@ -4,7 +4,7 @@ import com.synerset.unitility.unitsystem.common.Velocity;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,13 +14,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PhysicalMaxValidatorTest {
 
+    private final Validator validator = Validation.byDefaultProvider()
+            .configure()
+            .messageInterpolator(new ParameterMessageInterpolator())
+            .buildValidatorFactory()
+            .getValidator();
+
     private record ExclusiveSample(
             @PhysicalMax(value = "36.0km/h", inclusive = false) Velocity velocity
-    ) {}
+    ) {
+    }
 
     private record InclusiveSample(
             @PhysicalMax(value = "36.0km/h") Velocity velocity
-    ) {}
+    ) {
+    }
 
     @Test
     @DisplayName("should cause result in violations when range is not inclusive and input value is equals to max limit")
@@ -30,8 +38,7 @@ class PhysicalMaxValidatorTest {
         ExclusiveSample upperBound = new ExclusiveSample(Velocity.ofKilometersPerHour(36));
 
         // When
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
+
         Set<ConstraintViolation<ExclusiveSample>> internalBoundViolations = validator.validate(insideRange);
         Set<ConstraintViolation<ExclusiveSample>> upperBoundViolations = validator.validate(upperBound);
 
@@ -49,8 +56,6 @@ class PhysicalMaxValidatorTest {
         InclusiveSample exceededBound = new InclusiveSample(Velocity.ofKilometersPerHour(37));
 
         // When
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
         Set<ConstraintViolation<InclusiveSample>> lowerBoundViolations = validator.validate(lowerBound);
         Set<ConstraintViolation<InclusiveSample>> upperBoundViolations = validator.validate(upperBound);
         Set<ConstraintViolation<InclusiveSample>> exceededBoundViolations = validator.validate(exceededBound);
